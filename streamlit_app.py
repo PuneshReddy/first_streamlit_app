bea049ec3a953724b307205640510e6e6406352d
PuneshReddy <139639535+PuneshReddy@users.noreply.github.com>
2023-07-16 13:59:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,7 +48,7 @@ except URLError as e:
 streamlit.header("The fruit load list contains:")
 def get_fruit_load_list():
   with my_cnx.cursor() as my_cur:
-#    my_cur.execute("use warehouse compute_wh")
+    my_cur.execute("use warehouse compute_wh")
     my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
     my_data_row = my_cur.fetchall()
 
@@ -56,12 +56,11 @@ if streamlit.button('Get Fruit Load List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   my_data_rows = get_fruit_load_list()
   streamlit.dataframe(my_data_rows)
-streamlit.stop()  
+ 
   
-    
-
 def insert_row_snowflake(new_fruit):
   with my_cnx.cursor() as my_cur:
+    my_cur.execute("use warehouse compute_wh")
     my_cur.execute("insert into fruit_load_list values ('from streamlit')")
     return "Thanks for adding" + my_fruit
     
@@ -70,8 +69,8 @@ add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 if streamlit.button('Add a Fruit to the List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   back_from_function = insert_row_snowflake(add_my_fruit)
-  streamlit.text(back_from_function)
+  streamlit.text(back_from_function)    
   
-
+streamlit.stop() 
 
 
